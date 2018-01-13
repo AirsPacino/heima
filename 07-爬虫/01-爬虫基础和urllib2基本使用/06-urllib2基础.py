c020ac010b317111eca9e66952fd0efd6be67617
Pacino <airs_pacino@sina.com>
2018-01-13 18:11:21 +0800

    urllib2初步

--- 07-爬虫/01-爬虫基础和urllib2基本使用/06-urllib2基础.py
+++ 07-爬虫/01-爬虫基础和urllib2基本使用/06-urllib2基础.py
@@ -1,3 +1,21 @@
+# coding:utf-8
 # 在python3中变为了urllib.request
 # import urllib.request
+
+"""
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+
+"""
+import urllib2
+
+ua_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"}
+url = "http://www.baidu.com/"
+
+request = urllib2.Request(url, headers=ua_headers)
+
+http_obj = urllib2.urlopen(request)
+html = http_obj.read()
+
+print http_obj.getcode()
+#print(html)
 
